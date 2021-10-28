64e167c4c11e3f6f1916a62057241bdded858da7
Jacken <wyg132456@163.com>
2021-10-28 18:09:23 +0800

    音乐播放器功能优化，UI更改

--- tools/music/music.js
+++ tools/music/music.js
@@ -1,10 +1,10 @@
 window.onload = function () {
     var select = document.getElementById("sel-music");
-    select.onclick = function () {
+    select.onclick = function () {  // 选择音乐
         music.click();
     };
     var musicAdd = document.getElementById("add-music");
-    musicAdd.onclick = function () {
+    musicAdd.onclick = function () {  // 添加音乐
         musicAd.click();
     };
 
@@ -15,7 +15,7 @@ window.onload = function () {
     var musicDel = document.getElementById("music-del");
     var title = document.getElementsByTagName("title")[0];
 
-    function optionWrite() {
+    function optionWrite() {  // 更新select的option
         let options = "";
         for (let i = 0; i < musicList.length; i++) {
             options += "<option value='" + i + "'>" + musicList[i]["name"] + "</option>";
@@ -23,9 +23,68 @@ window.onload = function () {
         musicLi.innerHTML = options;
         musicDel.innerHTML = "<option value='-1'>-删除音乐-</option>" + options;
     };
-    function musicUpdate (musicObject) {
-        audio.src = URL.createObjectURL(musicObject);
+
+    var processBar = document.getElementById("process-bar-back");
+    var processBarBody = document.getElementById("process-bar-body");
+    var processBarHead = document.getElementById("process-bar-head");
+    var processLen = processBar.offsetWidth;
+    function musicUpdate (musicObject) {  // 播放音乐，同时更改页面标题，更新进度条
         title.innerText = musicObject['name'];
+        audio.src = URL.createObjectURL(musicObject);
+        processBarBody.style.transitionDuration = "0s";
+        processBarHead.style.transitionDuration = "0s";
+        processBarBody.style.width = "0";
+        processBarHead.style.left = "-5px";
+        setTimeout(function () {
+            processBarBody.style.transitionDuration = audio.duration + "s";
+            processBarHead.style.transitionDuration = audio.duration + "s";
+            processBarBody.style.width = "100%";
+            processBarHead.style.left = processLen - 5 + "px";
+        }, 100);
+    };
+    
+    processBar.onclick = function () {  // 拖动进度条
+        let left = event.clientX - processBar.offsetLeft - processBar.offsetParent.offsetLeft
+        let rate = left / processLen;
+        let time = rate * audio.duration;
+        audio.currentTime = time;
+        processBarBody.style.transitionDuration = "0s";
+        processBarHead.style.transitionDuration = "0s";
+        processBarBody.style.width = rate * 100 + "%";
+        processBarHead.style.left = left - 5 + "px";
+        setTimeout(function () {
+            if (!audio.paused) {
+                processBarBody.style.transitionDuration = audio.duration - time + "s";
+                processBarHead.style.transitionDuration = audio.duration - time + "s";
+                processBarBody.style.width = "100%";
+                processBarHead.style.left = processLen - 5 + "px";
+            };
+        }, 100);
+    };
+
+    var playPause = document.getElementById("play-pause");
+    var style = document.getElementById("play-button");
+    playPause.onclick = () => {
+        if (audio.currentTime == audio.duration) {
+            musicUpdate(musicList[musicLi.value]);
+            style.innerText = ".process-frame:hover #play-pause{background: url(./play.gif) center center;}"
+        } else if (audio.paused) {
+            audio.play();
+            let leftTime = audio.duration - audio.currentTime;  // 剩余的时间
+            processBarBody.style.transitionDuration = leftTime + "s";
+            processBarHead.style.transitionDuration = leftTime + "s";
+            processBarBody.style.width = "100%";
+            processBarHead.style.left = processLen - 5 + "px";
+            style.innerText = ".process-frame:hover #play-pause{background: url(./pause.gif) center center;}"
+        } else {
+            audio.pause();
+            let rate = audio.currentTime / audio.duration;  // 当前播放进度
+            processBarBody.style.transitionDuration = "0s";
+            processBarHead.style.transitionDuration = "0s";
+            processBarBody.style.width = rate * 100 + "%";
+            processBarHead.style.left = processLen * rate - 5 + "px";
+            style.innerText = ".process-frame:hover #play-pause{background: url(./play.gif) center center;}"
+        };
     };
 
     music.onchange = () => {
@@ -58,27 +117,45 @@ window.onload = function () {
 
     var last = document.getElementById("last");
     var next = document.getElementById("next");
+    var mod = document.getElementById("music-mod");
     last.onclick = () => {
         if (0 < musicLi.value && musicLi.value < musicList.length) {
             --musicLi.value;
             musicUpdate(musicList[musicLi.value]);
         };
     };
-    next.onclick = () => {
+    function nextNear () {
         if (0 <= musicLi.value && musicLi.value < musicList.length - 1) {
             ++musicLi.value;
             musicUpdate(musicList[musicLi.value]);
         };
     };
-    var mod = document.getElementById("music-mod");
+    next.onclick = () => {
+        switch (mod.value) {
+            case "0":
+                nextNear();
+                break;
+            case "2":
+                musicLi.value = Math.floor(Math.random() * musicList.length);
+                musicUpdate(musicList[musicLi.value]);
+                break;
+            default:
+                if (musicLi.value < musicList.length - 1) {
+                    nextNear();
+                } else {
+                    musicLi.value = 0;
+                    musicUpdate(musicList[0]);
+                };
+        };
+    }
     audio.onended = () => {  // 播放模式
         switch (mod.value) {
             case "0":
-                next.click();
+                nextNear();
                 break;
             case "1":
                 if (musicLi.value < musicList.length - 1) {
-                    next.click();
+                    nextNear();
                 } else {
                     musicLi.value = 0;
                     musicUpdate(musicList[0]);
@@ -89,7 +166,10 @@ window.onload = function () {
                 musicUpdate(musicList[musicLi.value]);
                 break;
             case "3":
-                audio.play();
+                musicUpdate(musicList[musicLi.value]);
+                break;
+            case "4":
+                style.innerText = ".process-frame:hover #play-pause{background: url(./play.gif) center center;}"
         };
     };
     musicLi.onchange = () => {
