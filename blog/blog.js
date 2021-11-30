0c1e820e16c8d90e41acfccb7642a89198dc9c8a
Jacken <wyg132456@163.com>
2021-11-30 21:54:42 +0800

    b tang gou

--- blog/blog.js
+++ blog/blog.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-    var maxPage = 10;  // 最大页数
+    var maxPage = 11;  // 最大页数
     var nowPage = location.href.split("=")[1] ? parseInt(location.href.split("=")[1]) : 1;  // 当前页数
 
     // 博客内容
