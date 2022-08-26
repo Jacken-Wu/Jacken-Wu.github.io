cfb3e4484f3d2db4edb71c890294eccbbaec87ec
Jacken <wyg132456@163.com>
2022-08-26 19:20:26 +0800

    Add Erhelp.

---
+++ Erhelp/script.js
@@ -0,0 +1,41 @@
+var showWindow = document.getElementById("show-window");
+
+document.getElementById("erhelp").onclick = () => {
+    showWindow.src = "./functionList/erhelp";
+}
+document.getElementById("recom-song").onclick = () => {
+    showWindow.src = "./functionList/recomSong";
+}
+document.getElementById("song").onclick = () => {
+    showWindow.src = "./functionList/song";
+}
+document.getElementById("add-song").onclick = () => {
+    showWindow.src = "./functionList/addSong";
+}
+document.getElementById("errand").onclick = () => {
+    showWindow.src = "./functionList/errand";
+}
+document.getElementById("add-food").onclick = () => {
+    showWindow.src = "./functionList/addFood";
+}
+document.getElementById("del-food").onclick = () => {
+    showWindow.src = "./functionList/delFood";
+}
+document.getElementById("food").onclick = () => {
+    showWindow.src = "./functionList/food";
+}
+document.getElementById("erlove").onclick = () => {
+    showWindow.src = "./functionList/erlove";
+}
+document.getElementById("store").onclick = () => {
+    showWindow.src = "./functionList/store";
+}
+document.getElementById("buy").onclick = () => {
+    showWindow.src = "./functionList/buy";
+}
+document.getElementById("pack").onclick = () => {
+    showWindow.src = "./functionList/pack";
+}
+document.getElementById("rename").onclick = () => {
+    showWindow.src = "./functionList/rename";
+}
